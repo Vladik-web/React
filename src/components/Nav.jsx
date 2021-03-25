0ba2ff8df0c87a37bb1e1a587ffcb5d85fc475dd
Sereda Vlad <seredavlad40@gmail.com>
2021-03-25 11:01:22 +0200

    12

---
+++ src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+const Nav = ()=>{
+    return (
+        <nav className="navigation">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">More</a>
+          </li>
+          <li>
+            <a href="#">Contacts</a>
+          </li>
+        </ul>
+      </nav>
+    );
+}
+export default Nav;
